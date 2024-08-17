4a13e553ebb39c6d0f158bd15b5af150d30cbe57
Suraqa Jafrani <m.suraqa@gmail.com>
2024-08-17 15:25:08 +0500

    fix

--- app/Http/Controllers/ProfileController.php
+++ app/Http/Controllers/ProfileController.php
@@ -32,8 +32,6 @@ class ProfileController extends Controller
      */
     public function update(ProfileUpdateRequest $request): RedirectResponse
     {
-        // dd($request->post());
-        // dd($request->user());
         $request->user()->fill($request->validated());
         if ($request->user()->isDirty('email')) {
             $request->user()->email_verified_at = null;
@@ -65,5 +63,5 @@ class ProfileController extends Controller
 
         return Redirect::to('/');
     }
-    
+
 }
