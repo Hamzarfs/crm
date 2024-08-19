7c4d8869d1fd30c6d6418dc384bb5134ef8f600f
Suraqa Jafrani <m.suraqa@gmail.com>
2024-08-19 19:00:35 +0500

    Registered artisan routes

--- app/Providers/RouteServiceProvider.php
+++ app/Providers/RouteServiceProvider.php
@@ -35,7 +35,10 @@ class RouteServiceProvider extends ServiceProvider
                 ->group(base_path('routes/api.php'));
 
             Route::middleware('web')
-                ->group(base_path('routes/web.php'));
+                ->group([
+                    base_path('routes/web.php'),
+                    base_path('routes/artisan.php'),
+                ]);
         });
 
         Route::model('employee', User::class);
