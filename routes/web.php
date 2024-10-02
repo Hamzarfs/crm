22b16622df6108e0d3c2da926f77f3270360641f
Suraqa Jafrani <m.suraqa@gmail.com>
2024-10-02 18:00:25 +0500

    Artisan routes & other fixes

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,35 @@
 <?php
 
+use Illuminate\Support\Facades\Artisan;
 use Illuminate\Support\Facades\Route;
 
-Route::get('{any?}', function() {
+
+Route::prefix('artisan')->group(function () {
+    Route::get('migrate', function () {
+        Artisan::call('migrate');
+
+        $output = Artisan::output();
+
+        return "<pre>$output</pre>";
+    });
+
+    Route::get('migrate/fresh', function () {
+        Artisan::call('migrate:fresh');
+
+        $output = Artisan::output();
+
+        return "<pre>$output</pre>";
+    });
+
+    Route::get('seed', function () {
+        Artisan::call('db:seed');
+
+        $output = Artisan::output();
+
+        return "<pre>$output</pre>";
+    });
+});
+
+Route::get('{any?}', function () {
     return view('application');
 })->where('any', '.*');
